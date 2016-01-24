7aaf91bf643cdc70f10f3d48f5ff8751df7e902a
Matthew Chung <mattpkqwe@gmail.com>
2016-01-23 20:35:23 -0500

    directories

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Cover Template for Bootstrap</title>
+    <title>OpenCards</title>
 
     <!-- Bootstrap core CSS -->
     <link href="./vendor/twbs/bootstrap/docs/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -76,8 +76,8 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="./vendor/twbs/bootstrap/docs/dist/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="./vendor/twbs/bootstrap/docs/assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
