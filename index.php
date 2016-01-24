304b974d75cde59845088fe2148662d5ef9cd98f
Matthew Chung <mattpkqwe@gmail.com>
2016-01-23 23:50:38 -0500

    Testing Php

--- index.php
+++ index.php
@@ -2,22 +2,15 @@
 <html>
 <head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
 	<title>OpenCards</title>
 </head>
 
 <body>
-<script>
-window.onload = function() {
-    if (window.jQuery) {  
-        // jQuery is loaded  
-        alert("Yeah!");
-    } else {
-        // jQuery is not loaded
-        alert("Doesn't Work");
-    }
-}</script>
-
-<?php echo("Okay"); ?>
+	<div class="container">
+		<div class = "header"><h1>opencards</h1></div>
+		<?php echo('hello' + htmlspecialchars($_GET["deck"])); ?>
+	</div>
 </body>
 
 </html>
