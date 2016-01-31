f6e85ec20b9e00345a021fce638bb1948928c8d8
Matthew Chung <mattpkqwe@gmail.com>
2016-01-30 23:23:08 -0500

    Javascript

--- index.php
+++ index.php
@@ -4,21 +4,28 @@
 	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
-	<title>OpenCards</title>
+	<title>Opencards</title>
 </head>
 
 <body>
 	<div class="container">
-		<div class = "header"><h1>OpenCards</h1></div>
+		<div class = "header"><h1>Opencards</h1></div>
 		<?php
 		require 'databaseInit.php';
-		
-		echo isset($_GET["DECK"]) ? "Set" : "Unset";
+		// $db
+		$getExists = $_GET["deck"];
+		echo isset($getExists) ? "Set" : "Unset";
 		echo '<br />';
 		echo "Deck get request: " . htmlspecialchars($_GET["deck"]);
+		?>
+		<script>
+		var getExists = <?php echo json_encode($getExists); ?>;
+		</script>
+		<?php
 		$db.close();
 		?>
-		Okay
+		<script> console.log(getExists); </script>
+		<h4> Made by Matthew Chung </h4>
 	</div>
 </body>
 
