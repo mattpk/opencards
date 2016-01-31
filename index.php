3e1098d85986ceb4a128a579586961f2582559ed
Matthew Chung <mattpkqwe@gmail.com>
2016-01-31 17:07:19 -0500

    mobile support

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 <head>
+	<meta content='width=device-width, initial-scale=1' name='viewport'/>
 	<link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="./stylesheets/main.css">
