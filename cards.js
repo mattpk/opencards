ad485e9c59d62b4911519a4db68b484238827fad
Matthew Chung <mattpkqwe@gmail.com>
2016-02-01 01:02:42 -0500

    adding buttons

--- cards.js
+++ cards.js
@@ -1,12 +1,17 @@
 if (getExists) {
 	// check if deck exists, put deck
+	// set up cards and buttons
 	$(".container").append("<span id = 'cardtitle'></span>");
 	$(".container").append("<div id='cardwrapper'><div id='card'></div></div>");
 	$("#card").append("<span id='cardtext'></span>");
 	$(".container").append("<div id='buttonwrapper'></div>");
-	$("#buttonwrapper").append("<div class='btn' id='prev'>back</div>");
-	$("#buttonwrapper").append("<div class='btn' id='flip'>flip</div>");
-	$("#buttonwrapper").append("<div class='btn' id='next'>next</div>");
+	$("#buttonwrapper:nth-of-type(1)").append("<div class='btn' id='prev'>back</div>");
+	$("#buttonwrapper:nth-of-type(1)").append("<div class='btn' id='flip'>flip</div>");
+	$("#buttonwrapper:nth-of-type(1)").append("<div class='btn' id='next'>next</div>");
+	$(".container").append("<div id='buttonwrapper'></div>");
+	$("#buttonwrapper:nth-of-type(2)").append("<div class='btn' id='flipall'>flip all cards</div>");
+	$("#buttonwrapper:nth-of-type(2)").append("<div class='btn' id='shuffle'>shuffle order</div>");
+
 
 	console.log("Getexists, now post requesting");
 
