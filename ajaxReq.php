7a5cce514f848ab8265030c8f5d5790c613e6fbd
Matthew Chung <mattpkqwe@gmail.com>
2016-01-31 22:02:23 -0500

    bugfixing ajax

--- ajaxReq.php
+++ ajaxReq.php
@@ -14,22 +14,22 @@ if($db->connect_errno > 0) {
 	die('Unable to connect to database [' . $db->connect_error . ']');
 }
 
-echo json_encode(array(array(1,"connected")));
-/*
 $req = htmlspecialchars($_POST['req']);
 
 $reply = array(array(1,"First"));
 
 if ($req = "names") {
-	if (!$result = $db->query("SELECT * FROM `Decks`")) {
+	if (!$result = $db->query("SELECT * FROM `decks`")) {
 		die('Unable to load deck list. [' . $db->connect_error . ']');
 	}
+	/*
 	while ($row = $result->fetch_assoc()) {
 		$reply[] = array($row['ID'], $row['NAME']);
 	}
+	*/
 	echo json_encode($reply);
 } else {
 	echo json_encode(array("Math" , "Science", "Test"));
 }
-*/
+
 ?>
