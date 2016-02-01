f64f5cf4b6f02c35929c8ef9a488b6a9d42e7c91
Matthew Chung <mattpkqwe@gmail.com>
2016-01-31 22:31:19 -0500

    adjusted css values

--- cards.js
+++ cards.js
@@ -5,11 +5,12 @@ if (getExists) {
 	$('#cardtext').text("You can do it. Finish this by tonight!");
 } else {
 	// get and write names
+	$(".container").append("Select a Deck to view: <br />");
 	$(".container").append("<ul></ul>");
 	$.post("ajaxReq.php", {req: "names"}).done(function(data) {
 		var names = JSON.parse(data);
 		for (var x = 0; x < names.length; x++) {
-			$("<ul>").append("<li><a href = './?deck=" + names[x][0] + "'>" + names[x][1] + "</a></li>");
+			$(".container ul").append("<li><a href = './?deck=" + names[x][0] + "'>" + names[x][1] + "</a></li>");
 		}
 	});
 }
