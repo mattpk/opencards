2ac3938f89f3f205ada745ed03d9df9af0c5e20d
Matthew Chung <mattpkqwe@gmail.com>
2016-02-01 21:15:54 -0500

    fixing for the last time

--- ajaxReq.php
+++ ajaxReq.php
@@ -63,23 +63,8 @@ if ($req === "names") {
 	$side = $flipped ? 'BACK' : 'FRONT';
 
 	$reply = array($flipped, $side, $id, $tableName, $cardid, $text, $side);
-	//echo json_encode($reply);
+	echo json_encode($reply);
 
-	/* create a prepared statement */
-	if (($result->num_rows > 0) && ($stmt = $mysqli->prepare("UPDATE " . $tableName . " SET " . $side . " = ? WHERE ID = ?"))) {
-
-	    /* bind parameters for markers */
-	    $stmt->bind_param('ss', $text, $cardid);
-
-	    /* execute query */
-	    $stmt->execute();
-	    
-	    if ($stmt->errno) {
-	      echo "FAILURE!!! " . $stmt->error;
-	    }
-	    else echo json_encode("Updated {$stmt->affected_rows} rows");
-	    $stmt->close();
-	}
 	
 } else if ($req === "new") {
 
